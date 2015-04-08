b480d7a507a135d506de458d8d04e214a6e9611b
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2015-04-08 15:06:15 +0200

    batch script now calls git directly, and fails if it can't

--- clonegh.bat
+++ clonegh.bat
@@ -1,2 +1,6 @@
 echo Cloning http://github.com/%1/%2.git...
-"%PROGRAMFILES(x86)%\Git\bin\git.exe" clone http://github.com/%1/%2.git -q
+git --help>NUL
+if not %ERRORLEVEL%==9009 (
+git clone http://github.com/%1/%2.git -q
+)
+else echo ERROR: could not find git.exe
