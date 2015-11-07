6070b1f519a323b66f63a75cc07957bd8db62686
Trond Einar Snekvik <trondesn@stud.ntnu.no>
2015-11-07 12:50:51 +0100

    streamline windows install

--- clonegh.bat
+++ clonegh.bat
@@ -1,6 +1,12 @@
-echo Cloning http://github.com/%1/%2.git...
-git --help>NUL
-if not %ERRORLEVEL%==9009 (
-git clone http://github.com/%1/%2.git -q
+if not exist %2 (
+    echo Cloning http://github.com/%1/%2.git...
+    git --help>NUL
+    if not %ERRORLEVEL%==9009 (
+        git clone http://github.com/%1/%2.git -q
+    ) else (echo ERROR: could not find git.exe)
+) else (
+    echo %2 Already exists, updating...
+    cd %2
+    git pull origin master
+    cd ..
 )
-else echo ERROR: could not find git.exe
--- install.bat
+++ install.bat
@@ -1,31 +1,56 @@
 @echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
 SET VIMDIR=%PROGRAMFILES(x86)%\vim\vimfiles
 
 SET CURRDIR=%CD%
 
 cls
+MKDIR old
 
-IF EXIST %VIMDIR% (
+echo Fetching submodules...
+git submodule update
+
+IF EXIST "%VIMDIR%" (
     echo Vim directory already exists, moving it to a temporary location.
-    MKDIR old
     MOVE "%VIMDIR%" "%CURRDIR%\old\vimfiles" >NUL
-    MOVE "%VIMDIR%\..\.vimrc" "%CURRDIR%\old\.vimrc" >NUL
     MKDIR "%VIMDIR%"
 )
+IF EXIST %VIMDIR%\..\.vimrc (MOVE "%VIMDIR%\..\.vimrc" "%CURRDIR%\old\.vimrc" >NUL )
 
 echo Making directories...
-MKDIR "%VIMDIR%"
-MKDIR "%VIMDIR%\autoload"
+IF NOT EXIST "%VIMDIR%" (MKDIR "%VIMDIR%")
+IF NOT EXIST "%VIMDIR%\autoload" (MKDIR "%VIMDIR%\autoload")
 
 echo Linking vimfiles...
 MKLINK /H "%VIMDIR%\..\.vimrc" .vimrc >NUL
-MKLINK /H "%VIMDIR%\autoload\pathogen.vim" "%CD%\vim-pathogen\autoload\pathogen.vim" >NUL
-
-REM fontinstall.vbs
+IF NOT EXIST "%VIMDIR%\autoload\pathogen.vim" (MKLINK /H "%VIMDIR%\autoload\pathogen.vim" "%CURRDIR%\vim-pathogen\autoload\pathogen.vim" >NUL )
 
 cd "%VIMDIR%"
 
-MKDIR bundle
+IF NOT EXIST bundle (MKDIR bundle)
 cd bundle
 echo Cloning repos...
 call %CURRDIR%\clonegh.bat tpope vim-fugitive
