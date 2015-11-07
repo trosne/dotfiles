e4b7cfb5c923ae5af6f986aba9a5ca50366d52e9
Trond Einar Snekvik <trondesn@stud.ntnu.no>
2015-11-07 12:25:28 +0100

    added generic ctags_init.bat

---
+++ ctags_init.bat
@@ -0,0 +1,1 @@
+ctags -R -a --languages=c,c++ -f %SDK_ROOT%\..\tags %SDK_ROOT%\components\device %SDK_ROOT%\components\libraries\util %SDK_ROOT%\components\softdevice\s110\headers %SDK_ROOT%\components\toolchain\gcc
